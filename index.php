715cdfb0c2ebe6bbfa1f6a47ff584254ad6c16f3
formation <formation@formation>
2021-04-04 01:01:45 +0200

    Définition of workspace

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
-<!doctype html>
 
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="author" content="Julien Besset">
-        <meta name="description" content="Curriculum Vitae">
-        <link rel="stylesheet" type="text/css" href="style.css">
-		<title>Julien BESSET - Développeur web</title>
-        <link rel="icon" href="icon.png">
-    </head>        
-</html>
+
