7a68e228d149a17c96976fe0e1a51742e3475735
formation <formation@formation>
2021-04-06 00:19:26 +0200

    Content definition

--- header.php
+++ header.php
@@ -1,19 +1,1 @@
-<!DOCTYPE html>
-
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="author" content="Julien Besset">
-        <meta name="description" content="CV - Développeur Web">
-        <title>Julien Besset - Développeur web</title>
-
-            <!-- Style -->
-        <link rel="stylesheet" type="text/css" href="style/mobile.css">
-        <link rel="stylesheet" type="text/css" href="style/style.css">
-
-            <!-- Favicon -->
-        <link rel="icon" href="icon.png">
-    </head>
-    
-
     
--- index.php
+++ index.php
@@ -1,2 +1,179 @@
+<!DOCTYPE html>
 
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Julien Besset">
+        <meta name="description" content="CV - Développeur Web">
+        <title>Julien Besset - Développeur web</title>
 
+            <!-- Style -->
+        <link rel="stylesheet" type="text/css" href="style/mobile.css">
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+
+            <!-- Favicon -->
+        <link rel="icon" href="images/icon.png">
+    </head>
+
+    <body>
+            <!-- Navbar -->
+        <nav class="nav-bar">
+            <text>Julien Besset</text>
+            <ul>
+                <li><a href="#home">Accueil</a></li>
+                <li><a href="#about">Présentation</a></li>
+                <li><a href="#skills">Compétences</a></li>
+                <li><a href="#experience">Parcours</a></li>
+                <li><a href="#projects">Réalisations</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+
+            <!-- Header -->
+        <header>
+            <p>Bonjour, moi c'est Julien et je suis</p>
+            <h1>Développeur web junior</h1>
+        </header>
+
+            <!-- Presentation -->
+        <h2 id="about">Je me présente</h2>
+        <ul>
+            <li>Nom : Julien BESSET</li>
+            <li>Âge : 29 ans</li>
+            <li>Vie actuellement à Toulouse</li>
+        </ul>
+        <p>présenter l'objectif de ce site et de mon parcours</p>
+        <button class="item" id="cv" type="submit" onclick="window.open('pdf/CV.pdf')">Télécharger mon CV</button>
+
+            <!-- Skills -->
+        <h2 id=skills>Compétences</h2>
+
+        <div>
+            <h3>Front-end</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+        </div>
+
+        <div>
+            <h3>Back-end</h3>
+            <ul>
+                <li>PHP</li>
+                <li>MySQL</li>
+                <li>API REST</li>
+                <li>API GraphQl</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Framework et CMS</h3>
+            <ul>
+                <li>Magento</li>
+                <li>Sales Force Commerce Cloud</li>
+                <li>WordPress</li>
+                <li>Symfony</li>
+                <li>Vue.js</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Web design</h3>
+            <ul>
+                <li>Photoshop</li>
+                <li>InDesign</li>
+                <li>Illustrator</li>
+                <li>Charte graphique</li>
+            </ul>
+        </div>
+        <div>
+            <h3>Environnement de travail</h3>
+            <ul>
+                <li>Systèmes d'exploitation : Linux Ubuntu, Windows, Mac OS</li>
+                <li>Workflow : Git, Docker, FileZilla, Postman, GitHub, GitLab</li>
+                <li>IDE: Visual Studio Code, PHPStorm, Eclipse</li>
+            </ul>
+        </div>
+
+            <!-- Experience -->
+        <h2 id="experience">Parcours profesionnel</h2>
+
+        <h3>Développeur web</h3>
+        <p>Clever-Age - Toulouse - depuis Février 2021</p>
+        <p>Développement back-end sur Magento 2</p>
+
+        <h3>Préparation opérationnelle à l'emploi - Développeur web</h3>
+        <p>Clever-Institut - Toulouse - Novembre 2020 à Février 2021
+
+        <h3>Compétences plus - Développeur web</h3>
+        <p>ADRAR - Toulouse - Décembre 2019 à Février 2020</p>
+
+        <h3>Attaché de direction</h3>
+        <p>Up'n Jump 82 - Montauban - Juillet 2017 à Septembre 2019</p>
+        <p>Imaginer, programmer et concevoir les offres commerciales et les événements.</p>
+        <p>Manager et former une équipe de 5 collaborateurs.</p>
+        <p>Définir et assurer la communication digitale de la structure.</p>
+        <p>Réaliser les supports de la communication interne et externe de l'entreprise.</p>
+        <p>Assurer la gestion des conflits avec la clientèle et répondre au mieux à leurs attentes.</p>
+        <p>Organiser l'animation du point de vente et l'accueil de la clientèle.</p>
+
+        <h3>Chargé de projet marketing</h3>
+        <p>Soccer5 - Montréal de Mars 2016 à Janvier 2017</p>
+        <p>Evaluer et restructurer le site internet de la société.</p>
+        <p>Réaliser les supports de communcation interne et externes de l'entreprise.</p>
+        <p>Définir et assurer la communication digitale de l'entreprise.</p>
+        <p>Participer à l'organisation d'évènements team building.</p>
+        <p>Programmer et organiser un événement d'initiation au footgolf.</p>
+        <p>Construire et lancer un programme de fidélité.</p>
+        <p>Créer et programmer un championnat de soccer intérieur.</p>
+
+        <h3>Chargé de projet événementiel</h3>
+		<p>Spacer's Toulouse Volley - Toulouse - Novembre 2014 à Juin 2015</p>
+        <p>Assurer la spectacularisation des rencontres à domicile du club.</p>
+        <p>Réaliser une veille concurrentielle autour des différents clubs de Ligue A.</p>
+        <p>Imaginer et organiser un tournoi inter-étudiant au Palais des Sports de Toulouse.</p>				
+
+        <h3>Master en ingénierie et management des organisations sportives</h3>
+        <p>Toulouse School of Management (2014 - 2016)</p>
+
+        <h3>Agent d'acceuil</h3>
+        <p>Classico Foot - Plaisance du Touch - 2013 à 2016</p>
+        <p>Accueillir, renseigner, servir et encaisser la clientèle.</p>
+        <p>Planifier les réservations téléphoniques et physiques.</p>
+        <p>Participer à l'organisation d'un championnat de football indoor.</p>
+        <p>Créer et organiser des stages de football pour enfants.</p>			
+		
+        <h3>Licence STAPS Mmanagement du sport</h3>
+        <p>Université Toulouse III - Paul Sabatier (2011 - 2014)</p>
+
+            <!-- Projects -->
+        <h2 id="projects">Réalisations</h2>
+
+            <!-- Contact -->
+        <h2 id="contact">Contactez-moi</h2>
+        
+        <p>07.69.78.58.66</p>
+        <p>julien.besset.pro@gmail.com</p>
+        <p>Je suis disponible sur Toulouse et ses alentour</p>
+
+		<h3>Formulaire de contact</h3>
+	
+        <form>
+            <p>Vous souhaitez me rencontrer ? Prenez contact avec moi en remplissant ce formulaire :</p>
+            <div class="nomprenom">
+                <input type="text" name="nom" id="nom" placeholder="Indiquez votre nom"/>
+                <input type="text" name="prenom" id="prenom" placeholder="Indiquez votre prénom"/>
+            </div>
+            <input class="email" type="mail" name="email" id="mail" placeholder="Renseignez votre email"/>
+            <p>Laissez votre message :</p>
+            <textarea class ="message" type="text" name="message" id="message" placeholder="Votre message..." rows="10" cols="50"></textarea>
+            <button>Envoyer</button>
+        </form>
+
+        <footer>
+            <a href="https://www.linkedin.com/in/julien-besset/">linkedin</a>
+            <p>Réalisé par Julien BESSET © 2021 - Tous droits réservés</p>
+        </footer>
+ 
+    </body>
