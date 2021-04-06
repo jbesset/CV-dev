fabf2659610d8eff9b2435418dd50d1f51e13f65
formation <formation@formation>
2021-04-06 22:34:20 +0200

    CSS whitout color

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
                 <li><a href="#home">Accueil</a></li>
                 <li><a href="#about">Présentation</a></li>
                 <li><a href="#skills">Compétences</a></li>
-                <li><a href="#experience">Parcours</a></li>
                 <li><a href="#projects">Réalisations</a></li>
+                <li><a href="#experience">Parcours</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
@@ -43,7 +43,7 @@
             <li>Vie actuellement à Toulouse</li>
         </ul>
         <p>présenter l'objectif de ce site et de mon parcours</p>
-        <button class="item" id="cv" type="submit" onclick="window.open('pdf/CV.pdf')">Télécharger mon CV</button>
+        <button class="cv" type="submit" onclick="window.open('pdf/CV.pdf')">Télécharger mon CV</button>
 
             <!-- Skills -->
         <h2 id=skills>Compétences</h2>
@@ -96,70 +96,99 @@
             </ul>
         </div>
 
-            <!-- Experience -->
-        <h2 id="experience">Parcours profesionnel</h2>
-
-        <h3>Développeur web</h3>
-        <p>Clever-Age - Toulouse - depuis Février 2021</p>
-        <p>Développement back-end sur Magento 2</p>
-
-        <h3>Préparation opérationnelle à l'emploi - Développeur web</h3>
-        <p>Clever-Institut - Toulouse - Novembre 2020 à Février 2021
-
-        <h3>Compétences plus - Développeur web</h3>
-        <p>ADRAR - Toulouse - Décembre 2019 à Février 2020</p>
-
-        <h3>Attaché de direction</h3>
-        <p>Up'n Jump 82 - Montauban - Juillet 2017 à Septembre 2019</p>
-        <p>Imaginer, programmer et concevoir les offres commerciales et les événements.</p>
-        <p>Manager et former une équipe de 5 collaborateurs.</p>
-        <p>Définir et assurer la communication digitale de la structure.</p>
-        <p>Réaliser les supports de la communication interne et externe de l'entreprise.</p>
-        <p>Assurer la gestion des conflits avec la clientèle et répondre au mieux à leurs attentes.</p>
-        <p>Organiser l'animation du point de vente et l'accueil de la clientèle.</p>
-
-        <h3>Chargé de projet marketing</h3>
-        <p>Soccer5 - Montréal de Mars 2016 à Janvier 2017</p>
-        <p>Evaluer et restructurer le site internet de la société.</p>
-        <p>Réaliser les supports de communcation interne et externes de l'entreprise.</p>
-        <p>Définir et assurer la communication digitale de l'entreprise.</p>
-        <p>Participer à l'organisation d'évènements team building.</p>
-        <p>Programmer et organiser un événement d'initiation au footgolf.</p>
-        <p>Construire et lancer un programme de fidélité.</p>
-        <p>Créer et programmer un championnat de soccer intérieur.</p>
-
-        <h3>Chargé de projet événementiel</h3>
-		<p>Spacer's Toulouse Volley - Toulouse - Novembre 2014 à Juin 2015</p>
-        <p>Assurer la spectacularisation des rencontres à domicile du club.</p>
-        <p>Réaliser une veille concurrentielle autour des différents clubs de Ligue A.</p>
-        <p>Imaginer et organiser un tournoi inter-étudiant au Palais des Sports de Toulouse.</p>				
-
-        <h3>Master en ingénierie et management des organisations sportives</h3>
-        <p>Toulouse School of Management (2014 - 2016)</p>
-
-        <h3>Agent d'acceuil</h3>
-        <p>Classico Foot - Plaisance du Touch - 2013 à 2016</p>
-        <p>Accueillir, renseigner, servir et encaisser la clientèle.</p>
-        <p>Planifier les réservations téléphoniques et physiques.</p>
-        <p>Participer à l'organisation d'un championnat de football indoor.</p>
-        <p>Créer et organiser des stages de football pour enfants.</p>			
-		
-        <h3>Licence STAPS Mmanagement du sport</h3>
-        <p>Université Toulouse III - Paul Sabatier (2011 - 2014)</p>
-
-            <!-- Projects -->
+        <!-- Projects -->
         <h2 id="projects">Réalisations</h2>
+
+            <!-- Experience -->
+        <div class=background>
+
+            <h2 id="experience">Parcours professionnel</h2>
+
+            <div class="work">
+                <h3>Développeur web</h3>
+                <p>Clever-Age - Toulouse - depuis Février 2021</p>
+                <p>Développement back-end sur Magento 2</p>
+            </div>
+
+            <div class=school>
+                <h3>Préparation opérationnelle à l'emploi - Développeur web</h3>
+                <p>Clever-Institut - Toulouse - Novembre 2020 à Février 2021</p>
+            </div>
+
+            <div class=school>
+                <h3>Compétences plus - Développeur web</h3>
+                <p>ADRAR - Toulouse - Décembre 2019 à Février 2020</p>
+            </div>
+
+            <div class="work">
+                <h3>Attaché de direction</h3>
+                <p>Up'n Jump 82 - Montauban - Juillet 2017 à Septembre 2019</p>
+                <p>Imaginer, programmer et concevoir les offres commerciales et les événements.</p>
+                <p>Manager et former une équipe de 5 collaborateurs.</p>
+                <p>Définir et assurer la communication digitale de la structure.</p>
+                <p>Réaliser les supports de la communication interne et externe de l'entreprise.</p>
+                <p>Assurer la gestion des conflits avec la clientèle et répondre au mieux à leurs attentes.</p>
+                <p>Organiser l'animation du point de vente et l'accueil de la clientèle.</p>
+                </div>
+
+            <div class="work">
+                <h3>Chargé de projet marketing</h3>
+                <p>Soccer5 - Montréal de Mars 2016 à Janvier 2017</p>
+                <p>Evaluer et restructurer le site internet de la société.</p>
+                <p>Réaliser les supports de communcation interne et externes de l'entreprise.</p>
+                <p>Définir et assurer la communication digitale de l'entreprise.</p>
+                <p>Participer à l'organisation d'évènements team building.</p>
+                <p>Programmer et organiser un événement d'initiation au footgolf.</p>
+                <p>Construire et lancer un programme de fidélité.</p>
+                <p>Créer et programmer un championnat de soccer intérieur.</p>
+            </div>
+
+            <div class="work">
+                <h3>Chargé de projet événementiel</h3>
+                <p>Spacer's Toulouse Volley - Toulouse - Novembre 2014 à Juin 2015</p>
+                <p>Assurer la spectacularisation des rencontres à domicile du club.</p>
+                <p>Réaliser une veille concurrentielle autour des différents clubs de Ligue A.</p>
+                <p>Imaginer et organiser un tournoi inter-étudiant au Palais des Sports de Toulouse.</p>
+            </div>
+            <div class=school>
+                <h3>Master en ingénierie et management du sport</h3>
+                <p>Toulouse School of Management (2014 - 2016)</p>
+            </div>
+
+            <div class="work">
+                <h3>Agent d'acceuil</h3>
+                <p>Classico Foot - Plaisance du Touch - 2013 à 2016</p>
+                <p>Accueillir, renseigner, servir et encaisser la clientèle.</p>
+                <p>Planifier les réservations téléphoniques et physiques.</p>
+                <p>Participer à l'organisation d'un championnat de football indoor.</p>
+                <p>Créer et organiser des stages de football pour enfants.</p>	
+            </div>
+            <div class=school>
+                <h3>Licence STAPS Mmanagement du sport</h3>
+                <p>Université Toulouse III - Paul Sabatier (2011 - 2014)</p>
+            </div>
+
+        </div>
 
             <!-- Contact -->
         <h2 id="contact">Contactez-moi</h2>
         
-        <p>+33 7 69 78 58 66</p>
-        <p>julien.besset.pro@gmail.com</p>
-        <p>Je suis disponible sur Toulouse et ses alentour</p>
+        <div class="phone_box">
+            <p>+33 7 69 78 58 66</p>
+        </div>
+        <div class="email_box">
+            <p>julien.besset.pro@gmail.com</p>
+        </div>
+        <div class="city_box">
+            <p>Je suis disponible sur Toulouse et ses alentours</p>
+        </div>
 
-		<h3>Formulaire de contact</h3>
-	
-        <form>
+		
+        <h3 id="form">Formulaire de contact</h3>
+        <form methode="POST">
+            <input type="text" name="firstname" placeholder="Entrez votre nom"/>
+            <input type="text" name="lastname" placeholder="Entrez votre prénom"/>
+            <input type="text" name="email" placeholder="Entrez votre email"/>
             <button>Envoyer</button>
         </form>
 
